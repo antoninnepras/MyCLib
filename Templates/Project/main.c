d3741ac7e1fcf3fdc71f8c4e01a6cb99d158183a
Antonin Nepras <tondanepras@gmail.com>
2022-10-01 00:01:27 +0200

    Added Templates

---
+++ Templates/Project/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#ifndef SUCCESS
+#define SUCCESS 0
+#endif
+
+#ifndef ERROR
+#define ERROR 1
+#endif
+
+
+int
+main(int argc, const char** argv)
+{
+
+  return SUCCESS;
+}
